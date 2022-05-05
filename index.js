4d9b00cf88da4467670671e7bb5f402b92a2dc20
Pablo Salas <pascifuentes@gmail.com>
2022-05-04 21:06:33 -0400

    Base MiniChat socket.io

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('mensaje', (data) => {
+    io.emit('mensaje', data);
+  });
+
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
